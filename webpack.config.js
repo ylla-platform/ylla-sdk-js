d0f3bcf0293b6bd4e1298914e96002355120cd8a
Anthony Ettinger <anthony@chovy.com>
2018-12-07 18:09:56 -0800

    add first layout for webpack, mocha and library

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 module.exports = {
+	entry: './lib/index.js',
+	output: {
+		path: __dirname,
+		filename: './dist/ylla-sdk.bundle.js',
+	},
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env']
           }
         }
-      }
+      },
+			{
+				test: /test\.js$/,
+				use: 'mocha-loader',
+				exclude: /node_modules/,
+			},
     ]
   }
 }; 
