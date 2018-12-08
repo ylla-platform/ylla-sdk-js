b656ee79ea5a86b1a712be15bdbfaeb62e532cf2
Anthony Ettinger <anthony@chovy.com>
2018-12-07 17:24:02 -0800

    stub for sdk with webpack, babel and karma

---
+++ karma.conf.js
@@ -0,0 +1,19 @@
+module.exports = (config) => {
+  config.set({
+    frameworks: ['jasmine'],
+
+    files: [
+      { pattern: 'src/test.js' }
+    ],
+
+    preprocessors: {
+      'src/test.js': ['webpack']
+    },
+
+    webpack: require('./webpack.config'),
+
+    reporters: ['dots'],
+
+    browsers: ['ChromeHeadless']
+  });
+};
